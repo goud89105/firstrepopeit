ab85bce86d28e45c98e70a915d2ba6bec9d10f8f
Simplilearn GitHub <siddam.bharat@simplilearn.net>
2023-05-29 15:46:26 +0530

    first commit

---
+++ src/main/java/com/gitproject/gitproject/App.java
@@ -0,0 +1,18 @@
+package com.gitproject.gitproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    public void add(String data)
+    {
+    	System.out.println("dummy data");
+    }
+}
