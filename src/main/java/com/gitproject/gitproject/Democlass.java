ce327e3f9be65922b4afb0afbb7fc757b14b5075
Simplilearn GitHub <siddam.bharat@simplilearn.net>
2023-05-29 16:22:02 +0530

    new class and method created

---
+++ src/main/java/com/gitproject/gitproject/Democlass.java
@@ -0,0 +1,14 @@
+package com.gitproject.gitproject;
+
+public class Democlass {
+	// this is new method adding to our  code
+	public void method1() {
+		System.out.println(" This is new logic ");
+	}
+
+	public static void main(String[] args) {
+     
+		
+	}
+
+}
